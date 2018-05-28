647ff0a190454e2bb05a57eb101b43b76e134590
keks <keks@cryptoscope.co>
2018-05-28 14:29:26 +0300

    better testing

---
+++ test/all/all.go
@@ -0,0 +1,9 @@
+package all
+
+import (
+	// imported only for side effects / registring testing helpers
+	_ "cryptoscope.co/go/margaret/test/all"
+
+	_ "cryptoscope.co/go/librarian/badger"
+	_ "cryptoscope.co/go/librarian/mapidx"
+)
--- test/all/all_test.go
+++ test/all/all_test.go
@@ -4,12 +4,6 @@ import (
 	"testing"
 
 	ltest "cryptoscope.co/go/librarian/test"
-
-	// imported only for side effects / registring testing helpers
-	_ "cryptoscope.co/go/librarian/badger"
-	_ "cryptoscope.co/go/librarian/mapidx"
-	_ "cryptoscope.co/go/margaret/mem"
-	_ "cryptoscope.co/go/margaret/offset"
 )
 
 func Test(t *testing.T) {
