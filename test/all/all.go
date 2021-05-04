2625d39f6ae76feccc95aebfc4eaeb40589be8cb
Henry <cryptix@riseup.net>
2021-05-04 17:00:55 +0200

    remove old offset format

--- test/all/all.go
+++ test/all/all.go
@@ -6,8 +6,5 @@ import (
 	// import to register testing helpers
 	_ "go.cryptoscope.co/margaret/mem/test"
 
-	// offset1 tests pass minus the reverse ones. please update to v2
-	// _ "go.cryptoscope.co/margaret/offset/test"
-
 	_ "go.cryptoscope.co/margaret/offset2/test"
 )
