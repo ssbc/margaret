36ec9a80fa569a881fcd9701d9832c7607c4f185
cblgh <cblgh@cblgh.org>
2022-02-16 10:21:04 +0100

    add test for local fork repro

--- multilog/test/main.go
+++ multilog/test/main.go
@@ -20,6 +20,8 @@ func SinkTest(f NewLogFunc) func(*testing.T) {
 
 func MultiLogTest(f NewLogFunc) func(*testing.T) {
 	return func(t *testing.T) {
+		// makes sure local fork reproduction doesn't make a reappearance
+		t.Run("GetFreshThenReopenAndLogSomeMore",  MultilogTestGetFreshLogCloseThenOpenAgain(f))
 		t.Run("MultiSimple", MultiLogTestSimple(f))
 		t.Run("Live", MultilogLiveQueryCheck(f))
 	}
